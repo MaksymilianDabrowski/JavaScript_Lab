8883089eb79ea1129b18961e848ee1537fd1db85
Maksymilian <maksymilian.dabrowski421@gmail.com>
2023-11-19 18:28:37 +0100

    Lab_03 dalej

--- Lab_03/drumkit_script.js
+++ Lab_03/drumkit_script.js
@@ -1,5 +1,7 @@
 document.addEventListener('keypress', onKeyPress)
 
+
+// Do klawiszy na klawiaturze "przypnij" możliwość odtwarzania dźwięków instrumentu (pliki dźwiękowe perkusji są w repo, możesz zastosować również dowolne inne)
 const KeyToSound = {
     'b': document.querySelector('#s1'),
     'c': document.querySelector('#s2'),
@@ -11,7 +13,6 @@ const KeyToSound = {
     't': document.querySelector('#s8'),
     'y': document.querySelector('#s9'),
 }
-
 function onKeyPress(event) {
     const sound = KeyToSound[event.key]
     playSound(sound)
@@ -20,3 +21,21 @@ function playSound(sound) {
     sound.currentTime = 0
     sound.play()
 }
+// Aplikacja ma możliwość nagrywania ścieżki dźwiękowej na 4 osobnych kanałach (zapamiętanie jedynie na czas działania aplikacji)?
+// dodać 4 kanały z możliwością nagrania dźwięku
+
+let channel1 = [];
+let channel2 = [];
+let channel3 = [];
+let channel4 = [];
+let currChannel = channel1;
+
+
+
+
+
+// Metronom
+// dźwięk tink.wav jako podstawowa nuta metronomu
+
+
+
